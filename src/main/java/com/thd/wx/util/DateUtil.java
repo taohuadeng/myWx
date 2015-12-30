522c3b6385d2ce4b0796ea355840bf4178cf997c
taofd <taofadeng@21tb.com>
2015-12-30 12:55:21 +0800

    测试……

--- src/main/java/com/thd/wx/util/DateUtil.java
+++ src/main/java/com/thd/wx/util/DateUtil.java
@@ -1,13 +1,7 @@
 package com.thd.wx.util;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.ParsePosition;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Random;
+import java.text.*;
+import java.util.*;
 
 /**
  * @Author:taofadeng
@@ -793,7 +787,27 @@ public class DateUtil {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(getNewDateByDate(new Date(),-1));
+        /*Calendar c_begin = new GregorianCalendar();
+        Calendar c_end = new GregorianCalendar();
+        DateFormatSymbols dfs = new DateFormatSymbols();
+        String[] weeks = dfs.getWeekdays();
+
+        c_begin.set(2002, 1, 1); //Calendar的月从0-11，所以4月是3.
+        c_end.set(2050, 12, 31); //Calendar的月从0-11，所以5月是4.
+
+        int count = 1;
+        c_end.add(Calendar.DAY_OF_YEAR, 1);  //结束日期下滚一天是为了包含最后一天
+
+        while(c_begin.before(c_end)){
+            System.out.println("第"+count+"周  日期："+new java.sql.Date(c_begin.getTime().getTime())+", "+weeks[c_begin.get(Calendar.DAY_OF_WEEK)]);
+
+            if(c_begin.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY){
+                count++;
+            }
+            c_begin.add(Calendar.DAY_OF_YEAR, 1);
+        }*/
+
+        List<String> list = new ArrayList<String>();
     }
 
     /**
--- src/test/java/com/thd/wx/study/WorkbookTest.java
+++ src/test/java/com/thd/wx/study/WorkbookTest.java
@@ -7,59 +7,47 @@ import jxl.Sheet;
 import org.apache.commons.lang.StringUtils;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.text.DateFormatSymbols;
+import java.util.*;
 
 /**
  * Excel生成需要的文件
  */
 public class WorkbookTest {
     public static void main(String[] args) throws Exception {
-        Workbook workbook = null;
-        InputStream inputStream = new FileInputStream("F:/456.xls");
-        workbook = Workbook.getWorkbook(inputStream);
-        Sheet sheet = workbook.getSheet(0);
-        int rsRows = sheet.getRows();
-        System.out.println(rsRows);
         List<String> list = new ArrayList<String>();
         String id = "";
-        for (int i = 0; i < rsRows; i++) {
-            Cell cell0 = sheet.getCell(0, i);
-            String itemId = cell0.getContents();
-            if (StringUtils.isEmpty(id)) {
-                id = "'" + itemId + "'";
-            } else {
-                id = id + ",'" + itemId + "'";
+
+        Calendar cBegin = new GregorianCalendar();
+        Calendar cEnd = new GregorianCalendar();
+        DateFormatSymbols dfs = new DateFormatSymbols();
+        String[] weeks = dfs.getWeekdays();
+
+        cBegin.set(2000, 0, 1); //Calendar的月从0-11，所以1月是0.
+        cEnd.set(2050, 11, 31); //Calendar的月从0-11，所以12月是11.
+
+        int count = 1;
+        cEnd.add(Calendar.DAY_OF_YEAR, 1);  //结束日期下滚一天是为了包含最后一天
+
+        while (cBegin.before(cEnd)) {
+            String uuid = UUID.randomUUID().toString().replace("-", "");
+            String startTime = new java.sql.Date(cBegin.getTime().getTime()) + " 00:00:00";
+            String endTime = new java.sql.Date(cBegin.getTime().getTime()) + " 23:59:59";
+            String sql = "INSERT INTO \"public\".\"t_cal_calendar\" " +
+                    "(\"calendar_id\", \"start_time\", \"end_time\", \"corp_code\", \"create_time\", " +
+                    "\"create_by\", \"last_modify_time\", \"last_modify_by\", \"opt_time\") " +
+                    "VALUES " +
+                    "('" + uuid + "', '" + startTime + "', '" + endTime + "', 'default', '" + new Date() + "'," +
+                    " 'admin', '" + new Date() + "', 'admin', '88888888');";
+            list.add(sql);
+
+            System.out.println("第" + count + "周  日期：" + new java.sql.Date(cBegin.getTime().getTime()) + ", " + weeks[cBegin.get(Calendar.DAY_OF_WEEK)]);
+            if (cBegin.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+                count++;
             }
 
-//            String sql = "SELECT \"count\"(*) FROM t_els_study_plan WHERE corp_code='ladeng.com' AND plan_status='LANCH';\n";
-//            list.add(sql);
+            cBegin.add(Calendar.DAY_OF_YEAR, 1);
         }
-
-        //安排课程数
-        //String sql = "SELECT corp_code,\"count\"(*) FROM t_els_study_plan WHERE corp_code IN(" + id + ") AND plan_status='LANCH' GROUP BY corp_code;";
-        //上传课程数
-        //String sql = "SELECT corp_code,\"count\"(*) FROM t_els_course_info WHERE corp_code IN(" + id + ") AND create_corp_code IN (" + id + ") GROUP BY corp_code;";
-        //资料中心内容上传数量
-        //String sql = "SELECT corp_code,COUNT (*) FROM t_km_knowledge WHERE opt_status= 'ENABLE' AND corp_code IN (" + id + ") GROUP BY corp_code;";
-        //资讯数量
-        //String sql = "SELECT corp_code,\"count\"(*) from t_cms_info_content where corp_code IN (" + id + ") AND status = 'enable' and author <> '时代光华' and tbc_id is null GROUP BY corp_code;";
-        //问答  是否修改 首页公告
-//        String sql = "SELECT corp_code from t_qa_system_config where ((announcement_content <> '（1）请先检索问题，勿重复提问；\n" +
-//                "（2）问题描述采用“标题+问题描述+解决目的”格式，并选对话题；\n" +
-//                "（3）务必遵守系统相关保密规定。' and announcement_content <> 'v4.js.announcementContentA\n" +
-//                "v4.js.announcementContentB\n" +
-//                "v4.js.announcementContentC') or announcement_content is null or announcement_content = '') AND corp_code IN (" + id + ");";
-
-        //String sql = "SELECT tp.corp_code,\"count\"(*) from t_qa_topic tp INNER JOIN t_qa_org_relating rel ON rel.object_id = tp.topic_id WHERE tp.corp_code IN (" + id + ") GROUP BY tp.corp_code;";
-        //有个性化登录的公司
-        //String sql = "SELECT max(oc.corp_code) FROM t_oms_login_page olp INNER JOIN t_oms_corp oc ON olp.corp_id = oc.corp_id WHERE oc.corp_code IN (" + id + ") GROUP BY olp.corp_id;";
-        //是否添加培训管理员
-        //String sql = "SELECT corp_code FROM t_uc_user_role_rel WHERE corp_code IN (" + id + ") AND role_id = '402880593604ea49013605e3208602f2' GROUP BY corp_code HAVING COUNT(user_id)>0 ;";
-        //是否添过角色
-        String sql = "SELECT corp_code,\"count\"(*) from t_oms_role where corp_code IN (" + id + ") AND  override_id is null and override_type is null GROUP BY corp_code ;";
-        list.add(sql);
 
         File file = new File("F:/select.sql");
         FileWriter fw = null;
