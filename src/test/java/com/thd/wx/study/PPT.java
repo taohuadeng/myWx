d57fccc6b415bf2d6cc8bdf85fdbba21e14af784
taofd <taofadeng@21tb.com>
2016-03-24 21:18:40 +0800

    PPT处理……

--- src/test/java/com/thd/wx/study/PPT.java
+++ src/test/java/com/thd/wx/study/PPT.java
@@ -3,8 +3,15 @@ package com.thd.wx.study;
 
 import org.apache.poi.hslf.extractor.PowerPointExtractor;
 import org.apache.poi.hslf.record.ColorSchemeAtom;
+import org.apache.poi.hslf.record.Record;
+import org.apache.poi.hslf.record.StyleTextProp9Atom;
+import org.apache.poi.hslf.usermodel.HSLFPictureData;
+import org.apache.poi.hslf.usermodel.HSLFShape;
 import org.apache.poi.hslf.usermodel.HSLFSlide;
 import org.apache.poi.hslf.usermodel.HSLFSlideShow;
+import org.apache.poi.sl.usermodel.FontCollection;
+import org.apache.poi.sl.usermodel.PictureData;
+import org.apache.poi.sl.usermodel.Resources;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -21,12 +28,22 @@ public class PPT {
 
     //一张幻灯片一张幻灯片地读取
     public static void readDoc2(InputStream is) throws IOException{
-        HSLFSlideShow ss=new HSLFSlideShow(is);
-        List<HSLFSlide> slides=ss.getSlides();
+        HSLFSlideShow slideShow=new HSLFSlideShow(is);
+        List<HSLFPictureData> pictureData = slideShow.getPictureData();
+        List<HSLFSlide> slides=slideShow.getSlides();
         for (HSLFSlide slide : slides) {
             //读取一张幻灯片的标题
             String title=slide.getTitle();
             System.out.println("标题:"+title);
+            List<HSLFShape> shapes = slide.getShapes();
+            StyleTextProp9Atom[] numberedListInfo = slide.getNumberedListInfo();
+            for (StyleTextProp9Atom styleTextProp9Atom : numberedListInfo) {
+                Record[] childRecords = styleTextProp9Atom.getChildRecords();
+                for (Record childRecord : childRecords) {
+                    childRecord.getRecordType();
+                }
+            }
+            RichTextRun
             ColorSchemeAtom colorScheme = slide.getColorScheme();
             int fillsColourRGB = colorScheme.getFillsColourRGB();
             //读取一张幻灯片的内容(包括标题)
@@ -38,25 +55,21 @@ public class PPT {
                 System.out.println(runs[j].getText());
             }*/
         }
-        /*for(int i=0;i<slides.length;i++){
-            //读取一张幻灯片的标题
-            String title=slides[i].getTitle();
-            System.out.println("标题:"+title);
-            //读取一张幻灯片的内容(包括标题)
-            TextRun[] runs=slides[i].getTextRuns();
-            for(int j=0;j<runs.length;j++){
-                System.out.println(runs[j].getText());
-            }
-        }*/
+
+        Resources resources = slideShow.getResources();
+        FontCollection fontCollection = resources.getFontCollection();
+        for (HSLFPictureData data : pictureData) {
+            PictureData.PictureType type = data.getType();
+        }
     }
 
     public static void main(String[] args){
         File file = new File("D:/linux命令行入门_开发定制版.ppt");
         try{
             FileInputStream fin=new FileInputStream(file);
-            String cont=readDoc1(fin);
-            System.out.println(cont);
-            fin.close();
+//            String cont=readDoc1(fin);
+//            System.out.println(cont);
+//            fin.close();
             fin=new FileInputStream(file);
             readDoc2(fin);
             fin.close();
